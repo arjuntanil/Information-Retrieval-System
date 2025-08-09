c31b882c2e734dfa2674f8023a18327983d01caa
arjuntanil <arjuntanil123gmail.com>
2025-08-09 21:11:29 +0530

    "Added env and setup files"

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',           # Package name
+    version='0.0.0',                # Version
+    author='Bappy',                  # Author name
+    author_email='arjuntanil123@gmail.com',  # Author email
+    packages=find_packages(),        # Automatically find all packages
+    install_requires=[]              # Dependencies list (from requirements.txt if needed)
+)
